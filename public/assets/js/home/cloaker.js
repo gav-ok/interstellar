423b7291859b087e49312334559faf13ea259ba1
gav-ok <122681437+gav-ok@users.noreply.github.com>
2023-03-30 16:47:36 -0400

    Update cloaker.js

--- public/assets/js/home/cloaker.js
+++ public/assets/js/home/cloaker.js
@@ -8,7 +8,7 @@ try {
 }
 
 if (!inFrame && !navigator.userAgent.includes("Firefox")) {
-    const popup = open("google", "_blank")
+    const popup = open("chrome://settings/", "_blank")
     if (!popup || popup.closed) {
         alert("Allow popups and redirects to hide this from showing up in your history.")
     } else {
